10c0e329e4fc0b1351ea0d3ba697c93bc2dc6abf
Dinura Sasmitha <info.dinura@gmail.com>
2026-01-04 16:38:26 +0530

    style: Add some styles

--- src/Components/SearchBar.jsx
+++ src/Components/SearchBar.jsx
@@ -48,3 +48,74 @@ const SearchBar = () => {
     </StyledWrapper>
   );
 };
+
+const StyledWrapper = styled.div`
+  .search-container {
+    display: flex;
+    align-items: center;
+    max-width: 800px;
+    height: 50px;
+    margin: 0 auto;
+    background-color: rgba(255, 255, 255); 
+    border-radius: 50px;
+    justify-content: space-between;
+  }
+
+  .search-dropdown {
+    font-family: "Afacad Flux", serif;
+    padding: 8px 15px;
+    border-radius: 25px;
+    border: none;
+    background-color: white;
+    font-size: 1.1rem;
+    cursor: pointer;
+  }
+  .search-dropdown option {
+    color: #333;
+    font-size: 1rem;
+    padding: 10px;
+  }
+
+  .search-dropdown:focus {
+    outline: none;  /* Remove the outline on focus */
+  }
+
+  .search-dropdown option:hover {
+    background-color:rgb(102, 19, 19); /* Light grey background */
+    color: #0056b3; /* Change text color on hover */
+  }
+
+  .search-input {
+  font-family: "Comfortaa", serif;
+  padding: 16px;
+  margin-left: 10px;
+  border-radius: 25px;
+  border-color: #ccc;
+  border: none;
+  width: 100%;
+  max-width: 500px;
+  font-size: 0.8rem;
+}
+
+.search-input:focus {
+  outline: none;  /* Remove the outline on focus */
+}
+
+
+  .search-button {
+    padding: 10px 30px;
+    margin-left: 10px;
+    background-color:rgb(19, 18, 18);
+    color: white;
+    border: none;
+    height: 50px;
+    border-radius: 0px 50px 50px 0px;
+    cursor: pointer;
+  }
+
+  .search-button:hover {
+    background-color:rgb(112, 112, 112);
+  }
+`;
+
+export default SearchBar;
